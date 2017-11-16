55e534c1e3e485fb450b3faea9fa5c787c081a76
Brian Shacklett <brian.shacklett@rackspace.com>
2017-11-16 04:50:59 +0000

    Add test for Get-CfnLogicalRsourceId

---
+++ Functions/Private/Get-CfnLogicalResourceId.Tests.ps1
@@ -0,0 +1,27 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe 'Get-CfnStackResources' {
+
+  function Get-CfnStackResources {}
+
+  Mock Get-CfnStackResources {
+    (
+      Get-Content -Raw -Path "$here\mock\cfnSecurityGroups.json" `
+      | ConvertFrom-Json
+    )
+  }
+
+  It 'Returns the Correct Logical ID' {
+
+    $testArgs =
+    @{
+      'PhysicalResourceId' = 'sg-ca1d50a2'
+      'StackName'          = 'Foo'
+      'Region'             = 'Bar'
+    }
+
+    Get-CfnLogicalResourceId @testArgs | Should Be 'NFSSecurityGroup'
+  }
+}
