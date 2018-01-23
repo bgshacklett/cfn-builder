983497c4db51cc1d22804f20ea54eb190258213c
Brian Shacklett <brian.shacklett@rackspace.com>
2018-01-23 06:08:42 +0000

    Handle complex peers
    Instead of configuring the peer directly in the hash literal, a second
    hashtable is created. If the peer is a complex type (not just a string
    or int), we reduce the object's properties to a new hashtable.
    Otherwise, the simple type wrapped in a hashtable with the proper key
    name.

--- Functions/Private/New-Ec2SecurityGroupRule.ps1
+++ Functions/Private/New-Ec2SecurityGroupRule.ps1
@@ -28,6 +28,7 @@ function New-Ec2SecurityGroupRule
     $CidrIp,
 
     [Parameter(ParameterSetName = 'CidrIpv6')]
+    [Amazon.EC2.Model.Ipv6Range]
     $CidrIpv6,
 
     [Parameter(ParameterSetName = 'DestinationPrefixListId')]
@@ -49,15 +50,32 @@ function New-Ec2SecurityGroupRule
     # The peer type is decided based on which peer parameter is passed in.
     $peerType  = $PsCmdlet.ParameterSetName
 
-    # We get the peer value from the value of the afore-mentioned parameter.
+    # We'll get the input dynamically based on the parameter set name.
     $peerValue = $PSBoundParameters.$peerType
+
+    # If we get a simple value, such as a string or an int, we need to wrap it
+    # in a Hashtable to allow combining it with the inputs we will be passing
+    # through.
+    if ($peerValue.Gettype() -in 'String','int')
+    {
+      # Wrap the value in a simple hashtable.
+      $peerHash = @{ $peerType = $peerValue }
+    }
+    else
+    {
+      # Reduce the properties of peerValue to a Hashtable
+      $peerHash = $peerValue.psobject.Properties `
+                  | ForEach-Object { $result = @{} } `
+                                   { $result += @{ $_.Name = $_.Value } } `
+                                   { $result }
+    }
 
     @{
       'IpProtocol' = $IpProtocol
       'FromPort'   = $FromPort
       'ToPort'     = $ToPort
-      "$peerType"  = $peerValue
-    }
+    } +
+    $peerHash
   }
 
   End {}
